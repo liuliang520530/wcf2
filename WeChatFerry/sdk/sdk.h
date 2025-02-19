cb5d48d079b7d8f44ffc55c25e945d3340f50f11
liuliangzheng <liuliangzheng520@163.com>
2025-02-19 19:46:11 +0800

    asdf

--- WeChatFerry/sdk/sdk.h
+++ WeChatFerry/sdk/sdk.h
@@ -1,4 +1,6 @@
 ﻿#pragma once
 
 int WxInitSDK(bool debug, int port);
+int WxInitSDK(DWORD pid, bool debug, int port);
+int WxInitSDK(const wchar_t *wxPath, bool debug, int port);
 int WxDestroySDK();
