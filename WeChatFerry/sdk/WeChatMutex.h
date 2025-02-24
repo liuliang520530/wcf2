d32e49742b17aa871108be351bd3ea2469513bf3
liuliangzheng <liuliangzheng520@163.com>
2025-02-24 17:34:42 +0800

    fasdfasdf

--- WeChatFerry/sdk/WeChatMutex.h
+++
@@ -1,9 +0,0 @@
-#ifndef WECHAT_MUTEX_H
-#define WECHAT_MUTEX_H
-
-#include <windows.h>
-
-// 关闭微信互斥锁，支持指定 PID 或遍历所有 WeChat.exe（默认遍历）
-int CloseWeChatMutex(DWORD targetPid = 0);
-
-#endif // WECHAT_MUTEX_H
